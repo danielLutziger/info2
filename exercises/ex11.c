babff94915bc9ac93f851f398e6b35d50263e5ab
daniellutziger <daniel.lutziger@uzh.ch>
2023-06-24 17:44:40 +0200

    lognest path

--- exercises/ex11.c
+++ exercises/ex11.c
@@ -1,3 +1,36 @@
 //
 // Created by danie on 24.06.2023.
 //
+
+
+#include <stdio.h>
+int longestPath(int x, int y, int matrix[x][y]){
+
+    int helper[x][y];
+    for(int i = 0; i < x; i++){
+        for(int j = 0; j < y; j++){
+            helper[i][j] = 1;
+        }
+    }
+
+    for(int p = 0; p < x; p++){
+        for(int q = 0; q < y; q++){
+            if(q > 0){
+                if(matrix[p][q] - matrix[p][q-1] >= -1 && matrix[p][q] - matrix[p][q-1] <= 1){
+                    helper[p][q] = helper[p][q-1]+1;
+                }
+            }
+            if(p > 0){
+                if(matrix[p][q] - matrix[p-1][q] >= -1 && matrix[p][q] - matrix[p-1][q] <= 1){
+                    helper[p][q] = helper[p-1][q]+1;
+                }
+            }
+        }
+    }
+    return helper[x-1][y-1];
+}
+int main(){
+    int matrix[4][6] = {{1,7,3,2,6,1},{2,5,4,5,9,3},{6,3,2,6,6,6},{8,7,5,4,8,7}};
+
+    printf("%d", longestPath(4,6, matrix));
+}
